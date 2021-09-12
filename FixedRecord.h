7382e2ac3b6b74bb0755cce6de48db47bc260b1b
XChanitoX <40049050+XChanitoX@users.noreply.github.com>
2021-09-12 14:17:35 -0500

    p1 Read Record

--- FixedRecord.h
+++ FixedRecord.h
@@ -1,59 +1,90 @@
 #ifndef LABORATORIO1_BD2_FIXEDRECORD_H
 #define LABORATORIO1_BD2_FIXEDRECORD_H
+
 #include <iostream>
-#include <vector>
 #include <fstream>
-#include <string>
+#include <vector>
+#include <cstring>
 
-using namespace std;
+struct Alumno {
 
-struct Alumno
-{
-    char codigo [5];
-    char nombre [11];
-    char apellidos [20];
-    char carrera [15];
-};
+    char codigo[5];
+    char nombre[11];
+    char apellidos[20];
+    char carrera[15];
 
-class FixedRecord{
-private:
-    string nombreArchivo;
-public:
-    FixedRecord(string nombreArchivo){
-        this->nombreArchivo = nombreArchivo;
+    Alumno() {
+        for (char & i : codigo)
+            i = ' ';
+        for (char & i : nombre)
+            i = ' ';
+        for (char & i : apellidos)
+            i = ' ';
+        for (char & i : carrera)
+            i = ' ';
     }
 
-    vector<Alumno> load(){
-        vector<Alumno> alumnos;
+    void imprimir() {
+        std::cout << "Codigo: " << codigo << "\n";
+        std::cout << "Nombre: " << nombre << "\n";
+        std::cout << "Apellidos: " << apellidos << "\n";
+        std::cout << "Carrera: " << carrera << "\n";
+        std::cout << "\n";
+    }
 
-        string line;
-        ifstream file(nombreArchivo); //Abre el archivo
-        if(file.is_open()){ //Verificamos que el archivo este abierto
-            while(getline(file,line)){//Leer linea por linea
-                cout << line << endl;
-            }
-            file.close();
-        }else{
-            cout << "El archivo no esta abierto" << endl;
+};
+
+class FixedRecord {
+
+private:
+    std::string fileName;
+
+public:
+    FixedRecord(const std::string nombreArchivo) {
+        this->fileName = nombreArchivo;
+    }
+
+    void add(Alumno alumno) {
+        std::ofstream File;
+        File.open(fileName, std::ios::app);
+        File.write((char *) &alumno, sizeof(alumno));
+        File.close();
+    }
+
+    std::vector<Alumno> load() {
+        std::vector<Alumno> alumnos;
+        std::ifstream File;
+        std::string registro;
+        File.open(fileName, std::ios::in);
+        while (getline(File, registro)) {
+            Alumno alumno;
+            strncpy(alumno.codigo, registro.substr(0, 4).c_str(), 5);
+            strncpy(alumno.nombre, registro.substr(5, 10).c_str(), 11);
+            strncpy(alumno.apellidos, registro.substr(16, 19).c_str(), 20);
+            strncpy(alumno.carrera, registro.substr(36, 14).c_str(), 15);
+            alumnos.push_back(alumno);
         }
-
+        File.close();
         return alumnos;
     }
 
-    void add(Alumno record, int pos){
-        fstream outFile;
-        outFile.open(this->nombreArchivo, ios::out | ios::binary);
-        if (outFile.is_open()) {
-            outFile.seekg(pos * sizeof(record), ios::beg);
-            outFile.write((char *) &record, sizeof(record));
-            outFile.close();
-        }else cout << "No se pudo abrir el archivo.\n";
+    Alumno readRecord(const int &pos) {
+        std::ifstream File;
+        std::string registro;
+        File.open(fileName, std::ios::in);
+
+        int physical = pos * (sizeof(Alumno) + 2);
+        File.seekg(physical);
+
+        getline(File, registro);
+        Alumno alumno;
+        strncpy(alumno.codigo, registro.substr(0, 4).c_str(), 5);
+        strncpy(alumno.nombre, registro.substr(5, 10).c_str(), 11);
+        strncpy(alumno.apellidos, registro.substr(16, 19).c_str(), 20);
+        strncpy(alumno.carrera, registro.substr(36, 14).c_str(), 15);
+
+        return alumno;
     }
-
-    Alumno readRecord(int pos){
-
-    }
-
 };
 
 #endif //LABORATORIO1_BD2_FIXEDRECORD_H
--- p1.cpp
+++ p1.cpp
@@ -5,15 +5,17 @@ using namespace std;
 
 int main(){
     FixedRecord funcionPrincipal = FixedRecord("../archivos/datos1.txt");
+
     cout << "Probando funcion load:" << endl;
     vector<Alumno> alumnos = funcionPrincipal.load();
-    cout << "Imprimiendo los alumnos del archivo..." << endl;
 
+    cout << endl << "Probando funcion add:" << endl;
 
-    cout << "Probando funcion add:" << endl;
-
-
-    cout << "Probando funcion readRecord:" << endl;
+    cout << endl << "Probando funcion readRecord:" << endl;
+    cout << "Imprimiendo el registro del alumno 3: " << endl;
+    Alumno alumno1 = Alumno();
+    alumno1 = funcionPrincipal.readRecord(2);
+    alumno1.imprimir();
 
     return 0;
 }
