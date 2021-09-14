6da0f157363adb51bbba3c69588afa2329e3b6f3
CEduardoSQUTEC <eduardo.castro@utec.edu.pe>
2021-09-13 23:45:53 -0500

    Donde

--- p4.cpp
+++ p4.cpp
@@ -1,13 +1,25 @@
 #include <fstream>
 #include <string>
+#include <utility>
 #include <vector>
 #include <utility>
+#include <iostream>
+#include <cassert>
 
 struct Matricula {
     std::string _codigo{};
     int _ciclo{};
     float _mensualidad{};
     std::string _observaciones{};
+
+    Matricula() = default;
+
+    Matricula(std::string codigo, int ciclo, float mensualidad, std::string observaciones) : _codigo(std::move(codigo)),
+                                                                                             _ciclo(ciclo),
+                                                                                             _mensualidad(mensualidad),
+                                                                                             _observaciones(
+                                                                                                     std::move(
+                                                                                                             observaciones)) {}
 };
 
 struct MetaDataMatricula {
@@ -16,10 +28,11 @@ struct MetaDataMatricula {
 };
 
 class VariableRecordBin {
-    std::string _filename;
+    std::string _filename{};
+    std::string _metafilename{};
 
 public:
-    VariableRecordBin(std::string  filename);
+    VariableRecordBin(std::string filename);
 
     std::vector<Matricula> load();
 
@@ -29,12 +42,18 @@ public:
 };
 
 VariableRecordBin::VariableRecordBin(std::string filename) : _filename(std::move(filename)) {
-    std::ofstream file("metadata." + _filename, std::ios::binary);
+    size_t indexDot = _filename.find_last_of(".");
+    _metafilename = _filename.substr(0, indexDot) + ".meta.bin";
+
+    std::ofstream metafile(_metafilename, std::ios::binary);
+    metafile.close();
+
+    std::ofstream file(_filename, std::ios::binary);
     file.close();
 }
 
 std::vector<Matricula> VariableRecordBin::load() {
-    std::ifstream metafile("metadata." + _filename, std::ios::binary | std::ios::ate);
+    std::ifstream metafile(_metafilename, std::ios::binary | std::ios::ate);
     int size = metafile.tellg(), nRecords = size / sizeof(MetaDataMatricula);
     metafile.seekg(0, std::ios::beg);
     std::vector<MetaDataMatricula> metaRecord(nRecords);
@@ -54,19 +73,19 @@ std::vector<Matricula> VariableRecordBin::load() {
 }
 
 void VariableRecordBin::add(Matricula record) {
-    std::ofstream file(_filename, std::ios::binary | std::ios::app | std::ios::ate);
+    std::ofstream file(_filename, std::ios::binary | std::ios::ate);
     int pos = file.tellp();
     file.write(reinterpret_cast<char *>(&record), sizeof(record));
     file.close();
 
-    std::ofstream metafile("metadata." + _filename, std::ios::binary | std::ios::app);
-    MetaDataMatricula metaDataMatricula = {pos, sizeof(record)};
+    std::ofstream metafile(_metafilename, std::ios::binary | std::ios::app);
+    MetaDataMatricula metaDataMatricula = {pos, (int) sizeof(record)};
     metafile.write(reinterpret_cast<char *> (&metaDataMatricula), sizeof(metaDataMatricula));
     metafile.close();
 }
 
 Matricula VariableRecordBin::readRecord(int pos) {
-    std::ifstream metafile("metadata." + _filename, std::ios::binary);
+    std::ifstream metafile(_metafilename, std::ios::binary);
     metafile.seekg(pos * sizeof(MetaDataMatricula), std::ios::beg);
     MetaDataMatricula metadata{};
     metafile.read(reinterpret_cast<char *>(&metadata), sizeof(metadata));
@@ -74,8 +93,26 @@ Matricula VariableRecordBin::readRecord(int pos) {
 
     std::ifstream file(_filename, std::ios::binary);
     file.seekg(metadata._pos, std::ios::beg);
+    Matricula matricula{};
+    file.read(reinterpret_cast<char *>(&matricula), metadata._size);
+    file.close();
 
+    return matricula;
+}
 
+int main() {
 
-    return Matricula;
+    std::cout << "=== P4 ===\n";
+
+    VariableRecordBin vrb01("../archivos/P4.bin");
+
+    Matricula matricula01("000", 3, 100.2, "No pago");
+    vrb01.add(matricula01);
+
+    auto record = vrb01.readRecord(0);
+
+    assert(record._observaciones == matricula01._observaciones);
+
+    std::cout << "Todo se ejecuto correctamente\n";
+    return 0;
 }
